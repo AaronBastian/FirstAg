6b5f766e9df07738b2abec4442728c740234c04b
Aaron Eversole <aeversole@agvance.net>
2013-12-29 12:39:51 -0600

    working on routing from kids page
    working on the router -- this is still a work in progress

--- FirstAG/FirstAG/App/viewmodels/kids.js
+++ FirstAG/FirstAG/App/viewmodels/kids.js
@@ -1,15 +1,25 @@
-﻿define(['services/logger'], function (logger) {
-    var vm = {
-        activate: activate,
-        title: 'Kids View'
-    };
+﻿define(['services/logger',
+    'durandal/plugins/routeConfig',
+    'durandal/plugins/router'],
+    function (logger, config, router) {
+    var vm = new function viewModel() {
+        var self = this;
+
+        self.title = 'Kids View';
+        self.friendsUrl = config.friends.url;
+        self.rangersUrl = config.rangers.url;
+        self.activate = function () {
+            logger.log('Kids View Activated', null, 'kids', true);
+            return true;
+        };
+
+        self.friendsClick = function () {
+            router.navigateTo(config.friends.url);
+        };
+        self.rangersClick = function () {
+            router.navigateTo(config.rangers.url);
+        };
+    }();
 
     return vm;
-
-    //#region Internal Methods
-    function activate() {
-        logger.log('Kids View Activated', null, 'kids', true);
-        return true;
-    }
-    //#endregion
 });
